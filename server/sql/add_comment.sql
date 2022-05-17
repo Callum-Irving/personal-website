e0c7fb6db878a1bb4f13df645476c6034f613af6
Callum Irving <callum.irving@protonmail.com>
2022-05-16 22:49:58 -0400

    remove server
    Since I switched to Utterances I no longer need a backend.

--- server/sql/add_comment.sql
+++
@@ -1,3 +0,0 @@
-INSERT INTO comment (post_slug, username, content, parent)
-VALUES ($1, $2, $3, $4)
-RETURNING username, content, posted, id;
--- server/sql/get_comments.sql
+++
@@ -1,4 +0,0 @@
-SELECT username, content, posted, parent, id
-FROM comment
-WHERE post_slug=$1
-ORDER BY parent ASC, id DESC;
