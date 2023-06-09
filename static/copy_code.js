f561d7d97be7d30aece91dc3a0eaa67652887526
Callum Irving <callum.irving@protonmail.com>
2023-06-09 12:24:58 -0400

    don't copy $ or trailing newlines

--- static/copy_code.js
+++ static/copy_code.js
@@ -15,7 +15,10 @@ async function copyCode(event) {
   const pre = btn.parentElement;
   let code = pre.querySelector("code");
   let text = code.innerText;
-  // TODO: Don't copy leading '$ ' (shell commands)
+  // Don't copy leading '$ ' (shell commands)
+  if (text.startsWith("$ ")) text = text.slice(2);
+  // Remove trailing newlines
+  if (text.endsWith("\n")) text = text.slice(0, text.length - 1);
   await navigator.clipboard.writeText(text);
   btn.blur();
   btn.innerText = "Copied!";
